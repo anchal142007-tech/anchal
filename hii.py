4fd754e412e88bd58e6f518c8b0e6db425f544ea
Adarsh Gupta <adarsh.gupta0131@gmail.com>
2026-08-04 12:42:41 +0530

    model added

--- hii.py
+++ hii.py
@@ -8,3 +8,8 @@ load_dotenv()
 my_api_key = os.getenv("GROQ_API_KEY")
 if not my_api_key:
     raise ValueError("GROQ_API_KEY environment variable is not set.")
+
+
+client = Groq(api_key=my_api_key)
+
+model_name = "llama-3.3-70b-versatile"
