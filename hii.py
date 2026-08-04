f34119f4b2827dbef61a6ba118c015028fd2fdfd
Adarsh Gupta <adarsh.gupta0131@gmail.com>
2026-08-04 12:31:09 +0530

    API key added

--- hii.py
+++ hii.py
@@ -4,3 +4,7 @@ from pathlib import Path
 from groq import Groq
 
 load_dotenv()
+
+my_api_key = os.getenv("GROQ_API_KEY")
+if not my_api_key:
+    raise ValueError("GROQ_API_KEY environment variable is not set.")
